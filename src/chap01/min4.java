4a83c25374c2df25f2b69c25a050efdf472bafae
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-03-13 20:44:01 +0900

    네 값의 최솟값
    네 값의 최솟값을 구해보자

---
+++ src/chap01/min4.java
@@ -0,0 +1,22 @@
+package chap01;
+
+public class min4 {
+
+	static int max4 (int a, int b, int c, int d) {
+		int max = a;
+		if(b < max)
+			max = b;
+		if(c < max)
+			max = c;
+		if(d < max)
+			max = d;
+		
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("max(3,2,1,4) = " + max4(3,2,1,4)); //[A] d>a>b>c
+
+	}
+
+}
