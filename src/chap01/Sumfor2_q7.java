3be0fbbfbc5e6670c3346df9d97d21d856339d87
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-03-22 20:29:41 +0900

    연습문제 7
    실습 1-5 프로그램을 참고하여 n이 7이면 '1+2+3+4+5+6+7=28' 로 출력하는 프로그램을 작성하세요.

---
+++ src/chap01/Sumfor2_q7.java
@@ -0,0 +1,21 @@
+package chap01;
+
+import java.util.Scanner;
+
+public class Sumfor2_q7 {
+		public static void main(String[] args) {
+			Scanner stdIn = new Scanner(System.in);
+			System.out.println("1부터n까지의 합을 구합니다.");
+			System.out.print("n의 값 : ");
+			int n = stdIn.nextInt();
+			
+			int sum = 0;
+			int i = 1;
+			for (i = 1; i < n; i++) {
+				sum += i;
+			System.out.print(i+"+");
+			}
+			sum += i;
+			System.out.print(n+"="+sum);
+		}
+}
