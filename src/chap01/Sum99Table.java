5dfa664ae56fd92e900506537eb3fde3196dc39e
JAEGEUN SONG <drow724@naver.com>
2021-03-26 16:37:54 +0900

    덧셈표
    곱셈이 아니라 덧셈을 출력하는 프로그램 작성

---
+++ src/chap01/Sum99Table.java
@@ -0,0 +1,17 @@
+package chap01;
+
+class Sum99Table {
+	public static void main(String[] args) {
+		System.out.print("   |");
+		for (int i = 1; i <= 9; i++)
+			System.out.printf("%3d", i);
+		System.out.println("\n---+---------------------------");
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.printf("%2d |", i);
+			for (int j = 1; j <= 9; j++)
+				System.out.printf("%3d", i + j);
+			System.out.println();
+		}
+	}
+}
