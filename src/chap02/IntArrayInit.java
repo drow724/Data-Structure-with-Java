6984ab96d923cf05b2ce9e96d632b3928f18aaa4
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-03-29 21:29:27 +0900

    배열 선언과 초기화
    배열 본체는 배열 초기화(array initializer)를 사용하면 배열 본체의 생성과 동시에 각 요소의 초기화가 가능하다.

---
+++ src/chap02/IntArrayInit.java
@@ -0,0 +1,14 @@
+package chap02;
+
+public class IntArrayInit {
+
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4,5}; //배열 초기자에 의해 생성
+		
+		for(int i = 0; i < a.length; i++) {
+			System.out.println("a[" + i + "] = " + a[i]);
+		}
+		
+	}
+
+}
