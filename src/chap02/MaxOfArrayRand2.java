98c519793b75c98b64ca0ad0c598697619c77963
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-03-30 01:20:48 +0900

    난수 사용해 배열의 요솟수 설정하기

---
+++ src/chap02/MaxOfArrayRand2.java
@@ -0,0 +1,34 @@
+package chap02;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MaxOfArrayRand2 {
+	
+	static int maxOf(int[] a) {
+		int max = a[0];
+		for (int i =1; i < a.length; i++)
+			if(a[i] > max)
+				max = a[i];
+		return max;
+	}
+	
+		public static void main(String[] args) {
+			Random rand = new Random();
+			Scanner stdIn = new Scanner(System.in);
+			
+			System.out.println("키의 최댓값을 구합니다.");
+			int num = 100+rand.nextInt(90);
+			
+			int[] height = new int[num];
+			
+			System.out.println("키의 값은 아래와 같습니다.");
+			for (int i = 0; i < num; i++) {
+				height[i] = 100+rand.nextInt(90);
+				System.out.println("height[" + i + "] : " + height[i]);
+			}
+
+			System.out.println("최댓값은 " + maxOf(height) + "입니다.");
+		}
+
+	}
